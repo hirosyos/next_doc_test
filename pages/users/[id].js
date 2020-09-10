f135163dc5059eacc7adf8022a4c8cc2e2ec8892
Yoshida Hiroshi <mu33devel1@gmail.com>
2020-09-10 15:50:57 +0900

    SSGに対応するコードにしてみた”

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -13,33 +13,37 @@ const UserPage = () => {
   // 本来はここでFireStoreアクセス;
   const dummyJsonData = getDummyJsonData(Number(id));
 
-  console.log({ dummyJsonData });
-  const dummyData2 = JSON.parse(dummyJsonData);
+  if (dummyJsonData) {
+    console.log({ dummyJsonData });
+    const dummyData2 = JSON.parse(dummyJsonData);
 
-  //   const dummyData2 = getDummyObjData(Number(id));
+    //   const dummyData2 = getDummyObjData(Number(id));
 
-  console.log({ dummyData2 });
+    console.log({ dummyData2 });
 
-  return (
-    <>
-      <p>URLに指定されたID: {id}</p>
-      <p>
-        {id}のid: {dummyData2.id}
-      </p>
-      <p>
-        {id}のname: {dummyData2.name}
-      </p>
-      <p>
-        {id}のhistory: {dummyData2.history}
-      </p>
-    </>
-  );
+    return (
+      <>
+        <p>URLに指定されたID: {id}</p>
+        <p>
+          {id}のid: {dummyData2.id}
+        </p>
+        <p>
+          {id}のname: {dummyData2.name}
+        </p>
+        <p>
+          {id}のhistory: {dummyData2.history}
+        </p>
+      </>
+    );
+  } else {
+    return <p>URLに指定されたID: {id} ロード中</p>;
+  }
 };
 
-export async function getServerSideProps() {
-  return {
-    props: {}, // will be passed to the page component as props
-  };
-}
+// export async function getServerSideProps() {
+//   return {
+//     props: {}, // will be passed to the page component as props
+//   };
+// }
 
 export default UserPage;
